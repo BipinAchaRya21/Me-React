e7ad86acd442333b506f3ac59f23328801165853
BipinAchaRya21 <bipinacharya981@gmail.com>
2026-08-19 12:44:57 +0545

    Create  React App practice

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./Chai"
+function App() {
+  return (
+    <>
+    <Chai/>
+    </>
+ //<h1>Hello, It's me Bipin Acharya!</h1>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai() {
+    return (
+        <h2>I am starting to learn React!</h2>
+    )
+}
+export default Chai
