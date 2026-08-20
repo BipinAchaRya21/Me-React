0d4c3e7762fcd6a2559bdc25d8967fd485ebbe48
BipinAchaRya21 <bipinacharya981@gmail.com>
2026-08-20 14:50:47 +0545

    Tailwind and props with components

---
+++ 03-tailwindProps/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <>
+   <h1 className='bg-blue-500 text-white p-4 rounded-xl mb-4'>Tailwind Test!</h1>
+    <Card username="BipinAcharya21" btnText="Visit Bipin"/>
+    <Card username="Kishor"/>
+    </>
+  )
+}
+
+export default App
